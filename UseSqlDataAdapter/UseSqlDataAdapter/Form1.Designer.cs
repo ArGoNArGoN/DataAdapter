a429246a0376ecd512159a69e352f2b523e90a76
ArGoN <mr.yayay01@bk.ru>
2021-05-07 02:41:26 +0300

    minor

---
+++ UseSqlDataAdapter/UseSqlDataAdapter/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace UseSqlDataAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.percentWin = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.raiting = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.teams = new System.Windows.Forms.DataGridView();
+            this.listTeams = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.percentWin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raiting)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.teams)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 249F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1104, 574);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.title);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.percentWin);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.raiting);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(861, 6);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(237, 562);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Редактирование или добавление";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(9, 480);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 73);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(6, 274);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(222, 30);
+            this.title.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PercentWin";
+            // 
+            // percentWin
+            // 
+            this.percentWin.DecimalPlaces = 2;
+            this.percentWin.Location = new System.Drawing.Point(6, 196);
+            this.percentWin.Name = "percentWin";
+            this.percentWin.Size = new System.Drawing.Size(118, 30);
+            this.percentWin.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Raiting";
+            // 
+            // raiting
+            // 
+            this.raiting.Location = new System.Drawing.Point(6, 120);
+            this.raiting.Name = "raiting";
+            this.raiting.Size = new System.Drawing.Size(118, 30);
+            this.raiting.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox2.Size = new System.Drawing.Size(843, 562);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 253F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.teams, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listTeams, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 115);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(831, 441);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // teams
+            // 
+            this.teams.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.teams.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.teams.Location = new System.Drawing.Point(256, 3);
+            this.teams.Name = "teams";
+            this.teams.Size = new System.Drawing.Size(572, 435);
+            this.teams.TabIndex = 2;
+            this.teams.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.teams_CellClick);
+            // 
+            // listTeams
+            // 
+            this.listTeams.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listTeams.FormattingEnabled = true;
+            this.listTeams.ItemHeight = 25;
+            this.listTeams.Location = new System.Drawing.Point(3, 3);
+            this.listTeams.Name = "listTeams";
+            this.listTeams.Size = new System.Drawing.Size(247, 435);
+            this.listTeams.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox3.Location = new System.Drawing.Point(6, 29);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox3.Size = new System.Drawing.Size(831, 86);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Элементы управления";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 38);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Редактирование";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(130, 121);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 29);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Пустое";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(130, 197);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 29);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Пустое";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 574);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.percentWin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raiting)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.teams)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DataGridView teams;
+        private System.Windows.Forms.ListBox listTeams;
+        private System.Windows.Forms.NumericUpDown raiting;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown percentWin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ UseSqlDataAdapter/UseSqlDataAdapter/Form1.cs
@@ -0,0 +1,239 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseSqlDataAdapter
+{
+	public partial class Form1 
+		: Form
+	{
+		public String ConnectionString { get; set; } = "Строку я не выложу в гит";
+		public NpgsqlConnection cn { get; set; }
+		public DataTable Table { get; set; }
+
+		public Boolean IsEdit { get; set; }
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+		/// ТУТ СТАРТ 
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			var strSql = "SELECT * FROM \"Teams\"";
+			
+			try
+			{
+				cn = new NpgsqlConnection(ConnectionString);
+
+				/// берем датаАдаптер и подключение
+				var dataAdapter = new NpgsqlDataAdapter(strSql, cn);
+				var table = new DataTable(); /// Создаем таблицу с данными
+
+				/// заполняем таблицу данными из БД через DataAdapter
+				dataAdapter.Fill(table); 
+				dataAdapter.Update(table); /// хз зачем, это не нужно
+
+				LoadDataGrid(table); /// скармливаем таблицу DataGrid
+				LoadList(table); /// скармливаем таблицу listBox
+
+
+				/// запоминаем это объект (он находится чуть-чуть выше (20 строчка))
+				Table = table;
+				/// ставим у таблицы Pr. Key 
+				Table.PrimaryKey = new DataColumn[1] { table.Columns[0] }; /// да-да, только массивом 
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		public void LoadList(DataTable table)
+		{
+			/// скармливаем таблицу
+			listTeams.DataSource = table;
+
+			/// Это не важно, но десь я установил отображение 
+			/// по свойству title и значению id (SelectedValue, теперь будет возвращать id)
+			listTeams.DisplayMember = "title";
+			listTeams.ValueMember = "id";
+		}
+		
+		public void LoadDataGrid(DataTable table)
+		{
+			/// скармливаем таблицу
+			teams.DataSource = table;
+
+			/// Это не важно
+			teams.RowHeadersVisible = false;
+			teams.Columns[0].Visible = false;
+			teams.Columns[1].Width = 150;
+			teams.Columns[2].Width = 80;
+			teams.Columns[3].Width = 100;
+			teams.Columns[4].Visible = false;
+			teams.Columns[5].Visible = false;
+		}
+
+		/// гинада смотреть
+		private void button1_Click(object sender, EventArgs e)
+		{
+			button3.Enabled = true;
+
+			button3.Text = "Добавить";
+			button3.Click += new System.EventHandler(this.button3_Click_1);
+			button3.Click -= new System.EventHandler(this.button3_Click);
+			IsEdit = false;
+		}
+
+		/// нинада смотреть x2
+		private void button2_Click(object sender, EventArgs e)
+		{
+			button3.Enabled = true;
+
+			button3.Text = "Сохранить изменения";
+			button3.Click += new System.EventHandler(this.button3_Click);
+			button3.Click -= new System.EventHandler(this.button3_Click_1);
+			IsEdit = true;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+        {
+			/// берем значения с формы и приводим к String
+			var title = this.title.Text; 
+			var raiting = checkBox1.Checked ? "NULL" : this.raiting.Value.ToString(); 
+			var percentWin = checkBox2.Checked ? "NULL" : this.percentWin.Value.ToString();
+
+			/// создаем запросик
+			var sql = $"UPDATE \"Teams\" SET \"Title\" = '{title}', \"Raiting\" = {raiting}, \"PercentWin\" = {percentWin} WHERE \"Id\"={teams.SelectedRows[0].Cells[0].Value}";
+            
+			try
+            {
+				cn.Open();
+				var com = new NpgsqlCommand(sql, cn);
+
+				/// я закоментил, ибо один раз случайно снес все данные из таблицы и.. блин, хочу сдохнуть, как же мне тогда было стыдно, что я не посмотрел в запрос и не увидел, что не прописал условие изменения строки 
+				/* if (com.ExecuteNonQuery() != 1)
+					throw new Exception("ну там запрос не выполнился, я не знаю почему, но мне как-то по..."); */
+
+				/// ищем строку по Pr. key (мы задали в 55 строчке) SelectedRows[0].Cells[0] - там хранится id
+				var row = Table.Rows.Find(teams.SelectedRows[0].Cells[0].Value);
+
+				/// Ищем столбец и заполняем его
+				row["Title"] = title;
+
+				/// Ищем столбец и заполняем его
+				if (checkBox1.Checked)
+					row["Raiting"] = DBNull.Value;
+				else
+					row["Raiting"] = this.raiting.Value;
+
+				/// Ищем столбец и заполняем его
+				if (checkBox1.Checked)
+					row["PercentWin"] = DBNull.Value;
+				else
+					row["PercentWin"] = this.percentWin.Value;
+			
+
+				/// и всо
+			}
+            catch (Exception ex)
+            {
+				MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+				cn.Close();
+            }
+		}
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+			var title = this.title.Text;
+			var raiting = checkBox1.Checked ? "NULL" : this.raiting.Value.ToString();
+			var percentWin = checkBox2.Checked ? "NULL" : this.percentWin.Value.ToString();
+
+			/// создаем запросик
+			var sql = $"INSERT INTO \"Teams\"(\"Id\", \"Title\", \"Raiting\", \"PercentWin\") VALUES (1001, '{title}', {raiting}, {percentWin})";
+			
+			try
+			{
+				cn.Open();
+				var com = new NpgsqlCommand(sql, cn);
+
+				/* if (com.ExecuteNonQuery() != 1)
+					throw new Exception("ну там запрос не выполнился, я не знаю почему, но мне как-то по..."); */
+
+				/// создаем строку
+				var row = Table.NewRow();
+
+				row["Id"] = 789; /// Заглушка у меня проблема с бд и я не могу вставить значения, поэтому костыль
+
+				/// Ищем столбец и заполняем его
+				row["Title"] = title;
+
+				/// Ищем столбец и заполняем его
+				if (checkBox1.Checked)
+					row["Raiting"] = DBNull.Value;
+				else
+					row["Raiting"] = this.raiting.Value;
+
+				/// Ищем столбец и заполняем его
+				if (checkBox1.Checked)
+					row["PercentWin"] = DBNull.Value;
+				else
+					row["PercentWin"] = this.percentWin.Value;
+
+				/// Добавляем строку в таблицу
+				Table.Rows.Add(row);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				cn.Close();
+			}
+		}
+
+		/// это нинада
+        private void teams_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+			teams.ClearSelection();
+			teams.Rows[e.RowIndex].Selected = true;
+
+            if (IsEdit)
+            {
+				percentWin.Value = 0;
+				raiting.Value = 0;
+
+				var s = teams.Rows[e.RowIndex];
+				title.Text = s.Cells[1].Value.ToString();
+
+
+				if (s.Cells[2].Value.ToString() == "")
+					checkBox1.Checked = true;
+				else
+					raiting.Value = (Decimal)s.Cells[2].Value;
+
+
+				if (s.Cells[3].Value.ToString() == "")
+					checkBox2.Checked = true;
+				else
+					percentWin.Value = (Decimal)s.Cells[3].Value;
+			}
+        }
+    }
+}
